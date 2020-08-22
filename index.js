881fd0a11c92009de2c49c38c9c7bc372ab405f4
Anas Aneeque <anasnew99@gmail.com>
2020-08-22 23:51:41 +0530

    Login Added

--- index.js
+++ index.js
@@ -1,11 +1,72 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 const session = require('express-session');
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
-require('dotenv').config();
-const process = require('process');
-
+const findOrCreate = require('mongoose-findorcreate');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+app.set('view engine', 'ejs');
+mongoose.connect(process.env.MONGODB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+},(err)=>{
+    if(!err){
+        console.log("Success: Connected to MongoDB atlas.");
+    }else{
+        console.log("Error: [MongoDB Connection] :\n",err);
+    }
+});
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+const eventSchema = new mongoose.Schema({
+    events : [String] ,
+    userId : String
+});
+eventSchema.plugin(findOrCreate);
+eventSchema.plugin(passportLocalMongoose);
+const Events = new mongoose.model("event", eventSchema);
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/calendar"
+  },
+  function(accessToken, refreshToken, profile,calendarData, cb) {
+    console.log("calendar Data",calendarData);
+    Events.findOrCreate({
+        events: calendarData,
+      userId: profile.id
+    }, function(err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get('/',(req,res)=>{
+res.render('login');
+})
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile','https://www.googleapis.com/auth/calendar.events.readonly']
+}));
+app.get('/auth/google/calendar',
+  passport.authenticate('google', {
+    failureRedirect: '/login'
+  }),
+  function(req, res) {
+    console.log("Succesfully Logged in With Google ");
+});
+
+app.listen(process.env.PORT||3000, function() {
+    console.log(" Server succesfully started ");
+});
