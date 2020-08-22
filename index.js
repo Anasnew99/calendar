9508c6a7bbb08d3b3c33bf3aab485bed276bf914
Anas Aneeque <anasnew99@gmail.com>
2020-08-23 01:21:38 +0530

    _

--- index.js
+++ index.js
@@ -69,7 +69,24 @@ passport.serializeUser(function(user, done) {
   });
 app.get('/',(req,res)=>{
     if(req.isAuthenticated()){
-        
+        var event = {
+            'summary': 'Google I/O 2015',
+            'location': '800 Howard St., San Francisco, CA 94103',
+            'description': 'A chance to hear more about Google\'s developer products.',
+            'start': {
+              'dateTime': '2015-05-28T09:00:00-07:00',
+              'timeZone': 'America/Los_Angeles',
+            },
+            'end': {
+              'dateTime': '2015-05-28T17:00:00-07:00',
+              'timeZone': 'America/Los_Angeles',
+            }
+        }
+        calendar.events.insert(event,(err,event)=>{
+            if(!err){
+                console.log("Wroked",event.htmlLink);
+            }
+        });
         res.send(calendar.events.get());
     }else{
         res.render('login');
